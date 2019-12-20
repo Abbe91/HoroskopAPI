488eb5115f467965d6bd45631d1123e2f3845ad6
Abbe91 <abdullah.abo.alhaijja@hotmail.com>
2019-12-20 15:44:05 +0100

    POST AND GET
    the post session work right now but I have to work more with get

--- javascript.js
+++ javascript.js
@@ -1,25 +1,55 @@
 
 
-$(document).ready(function(){
-    $("#createBtn").click(function(){
-        var name= $("#createTextbox").val();
-        // stutsText
-        $.post("horoscopeInfo.json",
-        {
-            name: name
-            // name : "Gemini",
-            // gloss: "The Twins",
-            // element: "Air",
-            // longitude_start: "60",
-            // longitude_end: "90"
-        },
-        function(data, status){
-            console.log('data', data);
-            console.log('status', status);
-            alert("Data: " + data + "\nStatus: " + status);
-        });
-         });
+function add() {
 
-});
+    let url = "./server/addhoroscope.php"
+    let method = "POST"
+    
+    let formData = new FormData()
+    let varde = document.getElementById('type').value;
+  
+    formData.set("date", varde)
+    makeRequest("./server/addhoroscope.php", "POST", formData, (result) => {
+        console.log(result)
+       // console.log($fromMonthAndDay)
+     })
+}
 
+function makeRequest(url, method, formData, callback){
+
+    fetch(url, {
+        method: 'post',
+        body: formData
+        }).then((response) => {
+        console.log(response)
+        return response.json()
+        }).then((result) => {
+        callback(result)
+        }).catch((err)=>{
+        console.log("Error: ", err)
+        })
+
+       
+    };
+
+function showData(){
+    let url = "./server/viewHoroscope.php"
+    let method ="GET"
+    fetch(url, {
+        method: method,
+
+    }).then((Response)=> {
+        return Response.json()
+    }).then((body)=> {
+        if(body){
+           echo ("hej");
+        }else{
+            echo ("hej");
+        }
+        let contant = document.getElementById("outPUT");
+        contant.innerText = body;
+    }).catch((err)=>{
+
+    })
+};  
 
